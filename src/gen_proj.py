5203f8d1d58b7801a9f44ff16dca230179dfd176
bbulger22 <bre00206@gmail.com>
2025-10-06 12:51:56 -0400

    updated demo proj

--- src/gen_proj.py
+++ src/gen_proj.py
@@ -1,25 +1,25 @@
 import random
 
-rx = random.randint(1, 100)
-ry = random.randint(1, 100)
-layers = random.randint(1, 1)
+rx = random.randint(1, 10)
+ry = random.randint(1, 10)
 
 with open("data.js", "w") as f:
     f.write('data = \'{"resolution" : [' + str(rx) + ', ' + str(ry) + '],"layers" : [')
-    for l in range(layers):
+    f.write('[')
+    for i in range(rx):
         f.write('[')
-        for i in range(rx):
+        for j in range(ry):
             f.write('[')
-            for j in range(ry):
-                f.write('[')
-                for k in range(3):
-                    f.write(str(random.random()))
-                    f.write(', ')
-                f.write("1")
-                f.write(']')
-                if (j != ry - 1): f.write(',')
+            f.write(str(i/rx))
+            f.write(', ')
+            f.write(str(j/ry))
+            f.write(', ')
+            f.write(str(0.5 * i/rx + 0.5 * j/ry))
+            f.write(', ')
+            f.write("1")
             f.write(']')
-            if (i != rx - 1): f.write(',')
+            if (j != ry - 1): f.write(',')
         f.write(']')
-        if (l != layers - 1): f.write(',')
+        if (i != rx - 1): f.write(',')
+    f.write(']')
     f.write("]}\';")  
